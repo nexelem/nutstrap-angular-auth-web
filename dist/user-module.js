b2c7eecfda296a5d151b2a45d0bbce9915fef0bd
Maria Rydzy <majrydzy@gmail.com>
2015-05-06 12:32:15 +0200

    Grunt configuration

--- dist/user-module.js
+++ dist/user-module.js
@@ -1,8 +1,1 @@
-(function() {
-  this.authModule = angular.module('myApp.auth', ['myApp.common', 'angular-flash.service', 'myApp.i18n', 'ngRoute']).config(function($routeProvider) {
-    return $routeProvider.when('/login', {
-      templateUrl: '/assets/lib/angular-auth-web/login/login.html'
-    });
-  });
-
-}).call(this);
+(function(){this.authModule=angular.module("myApp.auth",["myApp.common","angular-flash.service","myApp.i18n","ngRoute"]).config(function($routeProvider){return $routeProvider.when("/login",{templateUrl:"/assets/lib/angular-auth-web/login/login.html"})})}).call(this);
